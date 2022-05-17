bd893f2e5c0155e2116bdc00be7c8b3d6257c5e6
Stepan <105709091+Yaricheskyi@users.noreply.github.com>
2022-05-17 13:34:07 +0300

    Add files via upload

---
+++ Introduction_to_strings.py
@@ -0,0 +1,8 @@
+greeting = 'Hello!'
+first_name = 'Jack'
+last_name = 'White'
+exclamation_sign = '!'
+white_space = ' '
+whole_sentence = greeting + white_space + first_name + ' ' \
+    + last_name + exclamation_sign
+print(whole_sentence)
