4b174d84ec9b70fe31afe1b212541feb8e57225e
Stepan <105709091+Yaricheskyi@users.noreply.github.com>
2022-05-17 12:44:40 +0300

    Add files via upload

---
+++ python_escaping_and_triple_quotes .py
@@ -0,0 +1,19 @@
+# Escaping
+some_string = 'I\'m a programmer'
+print(some_string)
+another_string = 'i want to learn \"Python\"'
+print(another_string)
+
+string_with_new_lines = 'Hello!\n  \rMy name is Stepan'
+print(string_with_new_lines)
+
+numbers = '1\t23\t4567'
+print(numbers)
+some_text = '\t Hello! \nI\'m very glad to see you!'
+print(some_text)
+# Triple_quotes
+string_with_triple_quotes = '''This is ' text with "triple quotes"'''
+print(string_with_triple_quotes)
+another_string_with_triple_quotes = """This is ' text 
+with 'triple quotes'"""
+print(another_string_with_triple_quotes)
